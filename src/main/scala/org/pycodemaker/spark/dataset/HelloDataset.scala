34384c7f1d0596a024cba512595f698afb001f58
Jaafar Amine <pycodemaker@gmail.com>
2023-05-09 20:36:27 -0400

    :rocket: add dataset and sql examples

---
+++ src/main/scala/org/pycodemaker/spark/dataset/HelloDataset.scala
@@ -0,0 +1,44 @@
+package org.pycodemaker.spark.dataset
+import org.apache.log4j.Logger
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+case class SurveyRecord(Age: Int, Gender: String, Country: String, state: String)
+
+class HelloDataset extends Serializable {
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+
+    // create spark session
+    val spark = SparkSession.builder()
+      .appName("HelloDataset")
+      .master("local[3]")
+      .getOrCreate()
+
+    // read you csv file
+    val rawDF: Dataset[Row] = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("data/sample.csv")
+
+    // typesafe dataset
+    import spark.implicits._
+    val surveyDS:Dataset[SurveyRecord] = rawDF.select("Age", "Gender", "Country", "state").as[SurveyRecord]
+
+    //Type safe Filter
+    val filteredDS = surveyDS.filter(r => r.Age < 40)
+    //Runtime Filter
+    val filteredDF = surveyDS.filter("Age  < 40")
+
+    //Type safe GroupBy
+    val countDS = filteredDS.groupByKey(r => r.Country).count()
+    //Runtime GroupBy
+    val countDF = filteredDF.groupBy("Country").count()
+
+    logger.info("DataFrame: " + countDF.collect().mkString(","))
+    logger.info("DataSet: " + countDS.collect().mkString(","))
+
+
+  }
+
+}
---
+++ src/main/scala/org/pycodemaker/spark/sql/HelloSparkSql.scala
@@ -0,0 +1,29 @@
+package org.pycodemaker.spark.sql
+
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+
+object HelloSparkSql extends Serializable{
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+  def main(args: Array[String]): Unit = {
+    val spark = new SparkSession.Builder()
+      .appName("HelloSparkSQL")
+      .master("local[3]")
+      .getOrCreate()
+
+    val surveyDF = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("data/sample.csv")
+
+    surveyDF.createOrReplaceTempView("survey_table_view")
+    val q = "SELECT Country, count(1) AS count FROM survey_table_view WHERE AGE<40 GROUP BY Country"
+    val countDF = spark.sql(q)
+    logger.info(countDF.collect().mkString(","))
+
+
+  }
+
+
+
+}
